346362209340ebf55aa41f9a6b841c1b7a4ce465
BigC1109 <clanga1109@gmail.com>
2024-04-04 10:26:06 -0400

    Started work on the Generator Logic

---
+++ Dead_By_Devcade/GeneratorLogic.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dead_By_Devcade
+{
+    public class GeneratorLogic
+    {
+        public enum State
+        {
+            Complete,
+            Incomplete,
+            Regressing // Might not get to use this, but maybe in the future!
+        }
+
+        public State state;
+
+        private float progress;
+        private SkillCheckLogic skillie;
+
+
+        public GeneratorLogic()
+        {
+            this.state = State.Incomplete;
+            this.skillie = new SkillCheckLogic();
+        }
+
+        public void LoadContent(ContentManager contentManager)
+        {
+
+        }
+
+        public void Update(GameTime gametime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb, Rectangle windowSize)
+        {
+            //sb.DrawUserPrimatives(PrimitiveType.LineStrip, new Rectangle(new Point(), new Point()), null, Color.DarkGray);
+        }
+
+
+    }
+}
